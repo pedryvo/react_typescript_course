6e59e1d6302fedd9147ffba20911e1ee1965a225
Pedro Ivo <pedryvo@gmail.com>
2023-04-07 20:33:09 -0300

    feat: added react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>teste</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
